eceb271c8fe6a5edd45792beef32035664c44f6a
Shreyaan Pradhan <shreyaan.pradhan2020@vitstudent.ac.in>
2023-03-19 21:17:57 +0530

    made some changes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import Mainpage from './pages/mainpage';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import {ContextProvider} from "./Context"
 import Test from './pages/test';
+import { Helmet } from 'react-helmet';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
   <ChakraProvider>
-  <React.StrictMode>
+      <Helmet>
+      <title>Home</title>
+      <meta name="description" content="This is a portfolio website for Shreyaan Pradhan, B.Tech Computer Science Engineering. " />
+      <meta name="keywords" content="Portfolio, Shreyaan, Pradhan, shreyaan.codes, Shreyaan Pradhan, VIT Vellore, B.Tech, CSE, Computer Science, Web Developer, React developer, fontend developer" />
+      <meta name="robots" content="index, follow" />
+      <meta name="author" content="Shreyaan Pradhan" />
+      <link rel="canonical" href="https://www.shreyaan.codes/" />
+    </Helmet>
     <BrowserRouter>
     <ContextProvider>
       <Routes>
@@ -20,7 +28,6 @@ root.render(
       </Routes>
       </ContextProvider>
     </BrowserRouter>
-  </React.StrictMode>
   </ChakraProvider>
 
 );
